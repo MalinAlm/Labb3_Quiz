7ef69b50e33d537dfe14507407b6c9346f8bdad3
MalinAlm <malin.therese.almqvist@gmail.com>
2025-10-30 19:38:36 +0100

    Add project files.

---
+++ Labb3_Quiz/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Labb3_Quiz.Models;
+using Labb3_Quiz.ViewModels;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Labb3_Quiz
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int count = 1;
+        public MainWindow()
+        {
+           
+            InitializeComponent();
+
+            var pack = new QuestionPack("MyQuestionPack");
+            DataContext = new QuestionPackViewModel(pack);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            QuestionPackViewModel viewModel = (DataContext as QuestionPackViewModel);
+            viewModel.Name = "New name";
+            viewModel.Questions.Add(new Question($"Fråga {count++}", "2", "3", "1", "4"));
+        }
+    }
+}
