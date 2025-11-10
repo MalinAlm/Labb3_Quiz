7b4d7d1d5ed020c8c128123dce23dd9c0adcba4e
MalinAlm <malin.therese.almqvist@gmail.com>
2025-11-10 14:07:39 +0100

    Add PlayerView, toggle between views from menu

--- Labb3_Quiz/ViewModels/MainWindowViewModel.cs
+++ Labb3_Quiz/ViewModels/MainWindowViewModel.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Labb3_Quiz;
+using Labb3_Quiz.Command;
 
 namespace Labb3_Quiz.ViewModels
 {
@@ -29,16 +30,37 @@ namespace Labb3_Quiz.ViewModels
         public PlayerViewModel? PlayerViewModel { get; }
 		public ConfigurationViewModel? ConfigurationViewModel { get; }
 
-        public MainWindowViewModel()
+		private ViewModelBase _currentViewModel;
+		public ViewModelBase CurrentViewModel
 		{
-			
-			PlayerViewModel = new PlayerViewModel(this);
-			ConfigurationViewModel = new ConfigurationViewModel(this);
+			get => _currentViewModel;
+			set
+			{
+				_currentViewModel = value;
+				RaisePropertyChanged();	
+			}
+		}
+
+		public DelegateCommand ShowPlayerViewCommand { get; }
+		public DelegateCommand ShowConfigurationViewCommand {  get; }
+
+		public MainWindowViewModel()
+		{
 
             var pack = new QuestionPack("MyQuestionPack");
             ActivePack = new QuestionPackViewModel(pack);
-			ActivePack.Questions.Add(new Question($"Vad är 1+1", "2", "3", "1", "4"));
-			ActivePack.Questions.Add(new Question($"Vad heter Sveriges huvudstad?", "Stockholm", "Oslo", "London", "Berlin"));
+            ActivePack.Questions.Add(new Question($"Vad är 1+1", "2", "3", "1", "4"));
+            ActivePack.Questions.Add(new Question($"Vad heter Sveriges huvudstad?", "Stockholm", "Oslo", "London", "Berlin"));
+
+            ShowPlayerViewCommand = new DelegateCommand(_ => CurrentViewModel = PlayerViewModel);
+            ShowConfigurationViewCommand = new DelegateCommand(_ => CurrentViewModel = ConfigurationViewModel);
+
+            PlayerViewModel = new PlayerViewModel(this);
+			ConfigurationViewModel = new ConfigurationViewModel(this);
+
+			CurrentViewModel = ConfigurationViewModel;
+
+            
 
         }
 
